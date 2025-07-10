bbb644e3a056cea5016f4075890c1f90b213db16
Louwell Ranoco <louwellranoco@Louwells-MacBook-Air.local>
2025-07-10 11:23:26 +0800

    assignment

---
+++ assignment.py
@@ -0,0 +1,21 @@
+
+1
+
+numbers = ((5 + 6 + 9) - 9) 
+print (numbers)
+
+2
+
+people = {
+    'name': 'John', 'age': '35' 
+}
+print (people ['age'])
+
+3
+
+lists_places = ['Baguio', 'Bulacan', 'Pampanga', 'Manila']
+lists_places.pop(3)
+print (lists_places)
+
+lists_places.pop(2)
+print (lists_places)
